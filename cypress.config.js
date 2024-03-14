78fb32b47d1891de35f2690d7eb74afe63074ba0
Limuel II Menorca <limuel.il.a.menorca@accenture.com>
2024-03-14 15:14:05 +0800

    updated cypress.config.js & e2e.js: included setup for mochawesome report and set custom timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,41 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  execTimeout: 120000,
+  pageLoadTimeout: 120000,
+  requestTimeout: 120000,
+  responseTimeout: 120000,
+
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    overwrite: false,
+    charts: true,
+    reportPageTitle: 'Automation Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: true,
+    videoOnFailOnly: false,
+    reportDir: 'cypress/reports',
+  },
+
   e2e: {
+
+    experimentalModifyObstructiveThirdPartyCode: true,
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+
+    testIsolation: true,
+    experimentalStudio: true,
+    experimentalRunAllSpecs: true,
+    video: true,
+    chromeWebSecurity: false,
+    trashAssetsBeforeRuns: false,
+    screenshotOnRunFailure: false,
   },
 });
