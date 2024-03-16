5cc0b223266b53f62500c8d0b2cdf428ff6bb294
Limuel II Menorca <limuel.il.a.menorca@accenture.com>
2024-03-16 11:03:57 +0800

    completed the flow

--- cypress/e2e/1-my-profile/01_MyProfile_Edit_ContactInfo_Location_Languages.cy.js
+++ cypress/e2e/1-my-profile/01_MyProfile_Edit_ContactInfo_Location_Languages.cy.js
@@ -31,7 +31,7 @@ describe('E2E Test: User Profile Editing', () => {
         cy.get('.col-lg-8 > .sui-text-darkGray-lighter').contains('menorcalimuel@gmail.com').should('be.visible');
         cy.get('.tw-mt-1').contains('65f2973cbb4e7cf68cb8eca2').should('be.visible');
 
-        // Check first if the profile icon is displayed
+        // Check first if the profile icon is displayed then click
         cy.get('.sui-gap-2 > .sui-rounded-full > .sui-flex').contains('LM').should('be.visible');
         cy.get('.sui-gap-2 > .sui-rounded-full > .sui-flex').contains('LM').click();
 
@@ -56,46 +56,46 @@ describe('E2E Test: User Profile Editing', () => {
             .should('contain', 'menorcalimuel@gmail.com')
             .should('contain', '65f2973cbb4e7cf68cb8eca2');
 
-        // // Check if the contact information label is displayed
-        // cy.get('div').contains('Contact Information');
+        // Check if the contact information label is displayed
+        cy.get('div').contains('Contact Information');
 
-        // // Check the current phone number and save to a variable
-        // let phoneNumber;
+        // Check the current phone number and save to a variable
+        let phoneNumber;
 
-        // cy.get('p.sui-text-b3').invoke('text').then(text => {
-        //     phoneNumber = text.trim();
-        // });
+        cy.get('p.sui-text-b3').invoke('text').then(text => {
+            phoneNumber = text.trim();
+        });
 
-        // // Click the contact information edit button
-        // cy.get('.figma-section-header-text-margin > .col-lg-2 > .sui-rounded').click();
+        // Click the contact information edit button
+        cy.get('.figma-section-header-text-margin > .col-lg-2 > .sui-rounded').click();
 
-        // // Update the phone number
-        // cy.get('input[name="phone_number.line_number"]').clear().type('9152660387');
+        // Update the phone number
+        cy.get('input[name="phone_number.line_number"]').clear().type('9152660387');
 
-        // // Click save
-        // cy.get('.sui-gap-2 > .sui-c-btn-primary').click();
+        // Click save
+        cy.get('.sui-gap-2 > .sui-c-btn-primary').click();
 
-        // // Check if the phone number is updated
-        // cy.get('p.sui-text-b3').should('contain', '9152660387');
+        // Check if the phone number is updated
+        cy.get('p.sui-text-b3').should('contain', '9152660387');
 
-        // // Check the current postal code and save to a variable
-        // let postalCode;
+        // Check the current postal code and save to a variable
+        let postalCode;
 
-        // cy.get('.row div.col-auto.ml-7 p.sui-text-b3').invoke('text').then(text => {
-        //     postalCode = text.trim();
-        // });
+        cy.get('.row div.col-auto.ml-7 p.sui-text-b3').invoke('text').then(text => {
+            postalCode = text.trim();
+        });
 
-        // // Click the location edit button
-        // cy.get(':nth-child(1) > :nth-child(1) > .col-lg-2 > .sui-rounded').click();
+        // Click the location edit button
+        cy.get(':nth-child(1) > :nth-child(1) > .col-lg-2 > .sui-rounded').click();
 
-        // // Update the postal code
-        // cy.get('input[name="postalCode"]').clear().type('1777');
+        // Update the postal code
+        cy.get('input[name="postalCode"]').clear().type('1777');
 
-        // // Click save
-        // cy.get('.sui-gap-2 > .sui-c-btn-primary').click();
+        // Click save
+        cy.get('.sui-gap-2 > .sui-c-btn-primary').click();
 
-        // // Check if the postal code is updated
-        // cy.get('.row div.col-auto.ml-7 p.sui-text-b3').should('contain', '1777');
+        // Check if the postal code is updated
+        cy.get('.row div.col-auto.ml-7 p.sui-text-b3').should('contain', '1777');
 
         // Click languages
         cy.contains('a', 'Languages').click();
@@ -108,6 +108,7 @@ describe('E2E Test: User Profile Editing', () => {
 
         cy.get('p.sui-text-b3.tw-font-bold.sui-text-darkGray-darker').invoke('text').then(text => {
             primaryLanguage = text.trim();
+
         });
 
         // Click the primary language edit button
@@ -134,19 +135,29 @@ describe('E2E Test: User Profile Editing', () => {
         });
 
         // Click other languages edit button
-        cy.get('.edit-div').find('svg').click();
+        cy.get('.edit-div').click();
 
         // Click other languages dropdown
-        cy.get('.sui-c-floating-label-dropdown').click();
+        cy.get(':nth-child(1) > .sc-dlnjwi > .row > .col-lg-12 > .sui-c-input-dropdown-container > .sui-c-input-dropdown__control > .sui-c-input-dropdown__value-container').click();
 
         // Type another language
-        cy.get('.sui-c-floating-label-dropdown input').type('Arabic (Chad)');
+        cy.get(':nth-child(1) > .sc-dlnjwi > .row > .col-lg-12 > .sui-c-input-dropdown-container > .sui-c-input-dropdown__control > .sui-c-input-dropdown__value-container').type('Arabic (Chad)');
+        cy.focused().trigger('keydown', { keyCode: 9, which: 9 });
 
         // Click save
-        cy.get('button[type="submit"]').contains('Save').click();
+        cy.get('.sui-c-btn-primary').click();
 
         // Check if other languages is updated
-        cy.get('p.sui-text-b3.tw-font-bold.sui-text-darkGray-darker').should('not.contain', 'Other languages');
+        cy.get('p.sui-text-b3.tw-font-bold.sui-text-darkGray-darker').should('not.contain', 'Arabic (Chad)');
+
+        // Click my profile menu
+        cy.get('.sui-gap-2 > .sui-rounded-full > .sui-flex').contains('LM').click();
+
+        // Click sign out link
+        cy.get(':nth-child(3) > .sui-cursor-pointer > .sui-ml-sm').click();
+
+        // Check first if the user is navigated back to the sign in page and welcome text is displayed
+        cy.get('.md\\:px-7 > .sui-font-heading').contains('Welcome back!').should('be.visible');
 
     });
 });
